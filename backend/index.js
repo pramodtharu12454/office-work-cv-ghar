31f2a0789522bcd48a41bc24a937a24275687c1a
pramod tharu <tharupramod406@gmail.com>
2025-11-10 13:02:38 +0545

    initial project

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import connectDb from "./dbconnection.js";
+import { Usercontroller } from "./user/user.controller.js";
+
+const app = express();
+
+app.use(express.json());
+
+await connectDb();
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"], // allowed frontends
+    credentials: true, // allows cookies and auth headers
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // allowed headers
+  })
+);
+
+app.use(Usercontroller);
+
+app.use(express.json());
+const Port = 8000;
+
+app.listen(Port, () => {
+  console.log(`Server is running on port ${Port}`);
+});
---
+++ backend/user/user.schema.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true, // ensures no duplicate emails
+    trim: true,
+    lowercase: true,
+  },
+  phone: {
+    type: String,
+    trim: true,
+  },
+  address: {
+    type: String,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// ✅ Reuse existing model if already compiled
+const UserTable = mongoose.model("User", userSchema);
+
+export default UserTable;
